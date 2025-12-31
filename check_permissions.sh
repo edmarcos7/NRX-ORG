dd907be3823fa6b9bb30dfe97b9ac30a09aeaf4d
edmarcos7 <edmarcossantos05@gmail.com>
2025-12-30 22:32:16 -0300

    Create check_permissions.sh for app permission checks
    Add a script to check for overlay permissions in apps

--- check_permissions.sh
+++ check_permissions.sh
@@ -1,1 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/bash
 
+echo "---------- PERMISSÕES ----------"
+echo "Checando apps com permissão de sobreposição…"
+
+while IFS= read -r app; do
+    PERM=$(dumpsys package "$app" | grep "android.permission.SYSTEM_ALERT_WINDOW")
+    if [[ $PERM ]]; then
+        echo "⚠ Permissão de overlay encontrada em $app"
+    fi
+done < ../database/suspicious_apps.txt
