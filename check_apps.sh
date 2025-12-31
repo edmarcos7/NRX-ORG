9d391dd4fb20a804e61eea4ea3172f93568f2222
edmarcos7 <edmarcossantos05@gmail.com>
2025-12-30 22:30:20 -0300

    Create check_apps.sh to detect suspicious apps
    Added a script to check for suspicious installed apps.

--- check_apps.sh
+++ check_apps.sh
@@ -1,1 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
 
+echo "---------- APLICATIVOS INSTALADOS ----------"
+echo "Verificando apps suspeitos..."
+
+while IFS= read -r app; do
+    if pm list packages | grep -qi "$app"; then
+        echo "❌ App suspeito detectado: $app"
+    fi
+done < ../database/suspicious_apps.txt
